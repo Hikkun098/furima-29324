01f9b2f94cdaa0d59288586e8cc0a92ed08c14c0
Hikkun098 <mhhkn098@gmail.com>
2020-09-10 20:16:13 +0900

    ユーザーモデルのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, inclusion: { in: @ }
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :encrypted_password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  validates :birthday, presence: true,
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  validates :surname, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :phonetic_name1, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :phonetic_name2, presence: true, format: { with: VALID_NAME_REGEX }
+
 end
