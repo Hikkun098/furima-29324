2a7c6b3244c65a054044f963bade975574cd0849
Hikkun098 <mhhkn098@gmail.com>
2020-09-24 11:44:18 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :category_id, :status_id, :fee_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
